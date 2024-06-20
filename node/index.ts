b4894a806cb3eb0039048eb276f8a3d973569b54
pablopupulin <pablo.pupulin@vtex.com>
2024-06-20 03:15:54 -0300

    Adjust service

--- node/index.ts
+++ node/index.ts
@@ -1,10 +1,30 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import type {
+  ParamsContext,
+  RecorderState,
+  IOClients,
+  IOContext,
+  SegmentData,
+} from '@vtex/api'
 import { Service } from '@vtex/api'
 
 declare global {
+  interface CustomIOContext extends IOContext {
+    currentProfile: CurrentProfile
+    segment?: SegmentData
+    orderFormId?: string
+  }
+
+  interface CurrentProfile {
+    email: string
+    userId: string
+  }
+
   interface State {
     code: number
   }
 }
 
-export default new Service({})
+export default new Service<IOClients, RecorderState, ParamsContext>({
+  routes: {},
+})
